7f09966e49e6632a8971185a60235f1de8f4bb8d
Filip Sacha <filip.sacha@integral-learning.de>
2021-02-24 18:41:41 +0100

    formatting 3.0

--- locallib.php
+++ locallib.php
@@ -81,7 +81,7 @@ class locallib {
      * @param int $courseid The course to check
      * @return array array of MUMIE Modules
      */
-    public static function get_mumie_modules_by_course($courseid){
+    public static function get_mumie_modules_by_course($courseid) {
         global $DB;
         $mumiemodule = $DB->get_record(MODULE_TABLE, array("name" => 'mumie'));
         return $DB->get_records(COURSE_MODULE_TABLE, array("module" => $mumiemodule->id,  "course" => $courseid));
