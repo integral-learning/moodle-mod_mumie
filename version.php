e0139454a8ad517d6a8c92f36c7dcf7cd9a6fa12
Vasovasovasovaso <vasilije.nedeljkovic@integral-learning.de>
2022-07-27 10:48:18 +0200

    Find bug push

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2022070800; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2022070804; // The current module version (Date: YYYYMMDDXX).
 $plugin->component = 'mod_mumie'; // Full name of the plugin (used for diagnostics).
 $plugin->requires = 2020061500;
 $plugin->release = "v1.4.8";
