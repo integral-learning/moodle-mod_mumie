226511411618f34591193545f7464c80e5c7089a
tobias <tobias.goltz@integral-learning.de>
2023-02-02 16:20:24 +0100

    Plugin now requires moodle 4.0

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2023020107; // The current module version (Date: YYYYMMDDXX).
 $plugin->component = 'mod_mumie'; // Full name of the plugin (used for diagnostics).
-$plugin->requires = 2020061500;
+$plugin->requires = 2022041900;
 $plugin->release = "v1.4.9";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
