dafc7b23645c2dc91b93f1493e24e9c9667cc155
tobias <tobias.goltz@integral-learning.de>
2023-05-09 11:21:35 +0200

    Bumped version and updated changelog

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023022700; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2023050900; // The current module version (Date: YYYYMMDDXX).
 $plugin->component = 'mod_mumie'; // Full name of the plugin (used for diagnostics).
 $plugin->requires = 2022041900;
 $plugin->release = "v1.4.9";
