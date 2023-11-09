8172e2952dead4a499aabcf358c3dcacef8312b1
Filip <filip.sacha@integral-learning.de>
2023-11-09 18:43:57 +0100

    wip

--- classes/grades/synchronization/xapi_request.php
+++ classes/grades/synchronization/xapi_request.php
@@ -53,8 +53,6 @@ class xapi_request {
     public function send(): array {
         $ch = $this->create_post_curl_request();
         $result = (array) json_decode(curl_exec($ch));
-        error_log("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
-        error_log($ch);
         curl_close($ch);
         if ($this->has_error($result)) {
             return array();
